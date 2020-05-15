d606397a86680719e580fa71be4f52f741dec10b
Alasdair Clarke <a.clarke@essex.ac.uk>
2020-05-15 11:53:59 +0100

    Subgroup comparison tables.

--- analysis/scripts/import_subgroup_comparisons.R
+++ analysis/scripts/import_subgroup_comparisons.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 
 
-index_dat  <- read_csv("../../subgroups/subgroup_relations.csv")
-normal_dat <- read_csv("../../subgroups/subgroup_normal.csv")
+index_dat  <- read_csv("../subgroups/subgroup_relations.csv")
+normal_dat <- read_csv("../subgroups/subgroup_normal.csv")
 
 # sort out group names
-group_names  <- index_dat$group
+group_names  <- str_to_upper(index_dat$group)
 index_dat  <- index_dat[, -1]
 normal_dat <- normal_dat[, -1]
 
@@ -23,17 +23,25 @@ subgroups <- tibble(
 for (ii in 1:nrow(index_dat)) {
     for (jj in 1:ncol(index_dat)) {
 
-
         subgroups <- add_row(subgroups, 
-            group = group_names[ii], 
-            subgroup = group_names[jj], 
+            group = group_names[jj], 
+            subgroup = group_names[ii], 
             index = as.numeric(index_dat[ii,jj]),
             normal = normal_dat[ii,jj] == 1)
     }
 } 
 
+subgroups %>% 
+    select(group, subgroup, index) %>% 
+    pivot_wider(names_from = group, values_from = index) -> index_table
+
+subgroups %>% 
+    select(group, subgroup, normal) %>% 
+    pivot_wider(names_from = group, values_from = normal) -> normal_table
+
 # filter by index to remove all non-valid relations
 # also remove all indentity relationships
 subgroups %>% filter(
     is.finite(index),
-    group != subgroup) -> subgroups
+    group != subgroup) %>%
+    mutate(label = paste(group, "$\\rightarrow$", subgroup)) -> subgroups
